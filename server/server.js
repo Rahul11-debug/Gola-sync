c20ec34bcf2ea3a8f78c8c17d9f34683c8356965
Rahul11-debug <rahul.kumarundergrade@gmail.com>
2026-05-17 22:01:00 +0530

    server file updated

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: process.env.CLIENT_URL || 'http://localhost:5173' || 'https://goal-sync-ashen.vercel.app',
   credentials: true,
 }));
 app.use(express.json());
